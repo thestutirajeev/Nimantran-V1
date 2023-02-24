85924f503b83e0276609499aefe0284cb9134f5b
Stuti Rajeev <stutirajeev5@gmail.com>
2023-02-24 07:54:33 +0530

    Get Details Fragment updated

--- app/src/main/java/com/example/nimantran/ui/authentication/GetDetailsFragment.kt
+++ app/src/main/java/com/example/nimantran/ui/authentication/GetDetailsFragment.kt
@@ -5,14 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.navigation.fragment.findNavController
 import com.example.nimantran.R
+import com.example.nimantran.databinding.FragmentGetDetailsBinding
+
 class GetDetailsFragment : Fragment() {
+    private var _binding: FragmentGetDetailsBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_details, container, false)
+        _binding = FragmentGetDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textViewPrivacyPolicy.setOnClickListener {
+            findNavController().navigate(R.id.action_getDetailsFragment_to_privacyPolicyFragment)
+        }
+        binding.buttonSave.setOnClickListener {
+            if(binding.checkBoxPrivacyPolicy.isChecked == false){
+                makeText(requireContext(), "Please accept the privacy policy", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //code to save the details
+            }
+        }
     }
 
     companion object {
