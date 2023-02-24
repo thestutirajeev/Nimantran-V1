26524e90d7176e4137c5dec14e3fcd4218b6bc1f
Stuti Rajeev <stutirajeev5@gmail.com>
2023-02-25 00:47:39 +0530

    push

---
+++ app/src/main/java/com/example/nimantran/ui/admin/GiftDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.example.nimantran.ui.admin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nimantran.R
+import com.example.nimantran.databinding.FragmentNotificationListBinding
+
+class GiftDetailFragment : Fragment() {
+    private lateinit var binding: FragmentNotificationListBinding
+    private var _binding: FragmentNotificationListBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNotificationListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+    }
+}
