54722895852a034facbccf99476950d50265b1ba
Stuti Rajeev <stutirajeev5@gmail.com>
2023-02-24 10:34:45 +0530

    change1

---
+++ app/src/main/java/com/example/nimantran/ui/admin/UserListFragment.kt
@@ -0,0 +1,24 @@
+package com.example.nimantran.ui.admin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nimantran.R
+import com.example.nimantran.databinding.FragmentUserListBinding
+
+class UserListFragment : Fragment() {
+    private lateinit var binding: FragmentUserListBinding
+    private var _binding: FragmentUserListBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentUserListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+    }
+}
