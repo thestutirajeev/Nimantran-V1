d9f607c9ca9d9baf03b2f719d12b1fd858e795b8
Stuti Rajeev <stutirajeev5@gmail.com>
2023-03-07 12:02:34 +0530

    push 22

--- app/src/main/java/com/example/nimantran/ui/admin/AdminDashboardFragment.kt
+++ app/src/main/java/com/example/nimantran/ui/admin/AdminDashboardFragment.kt
@@ -6,10 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.get
 import androidx.navigation.fragment.findNavController
 import com.example.nimantran.AuthenticationActivity
-import com.example.nimantran.MainActivity
 import com.example.nimantran.R
 import com.example.nimantran.databinding.FragmentAdminDashboardBinding
 import com.google.firebase.auth.FirebaseAuth
--- app/src/main/java/com/example/nimantran/ui/main/HomeFragment.kt
+++ app/src/main/java/com/example/nimantran/ui/main/HomeFragment.kt
@@ -6,14 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nimantran.R
+import com.example.nimantran.databinding.FragmentHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.homebottombutton -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.designsbottombutton -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.drawerbottombutton -> {
+                    // Respond to navigation item 3 click
+                    true
+                }
+                else -> false
+            }
+        }
     }
     companion object {
     }
