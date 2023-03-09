e84148660afa796e5b5328ccf2f42b25dfb786f9
Stuti Rajeev <stutirajeev5@gmail.com>
2023-03-09 17:22:08 +0530

    push

---
+++ app/src/main/java/com/example/nimantran/ui/main/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.nimantran.ui.main.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.nimantran.R
+import com.example.nimantran.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+    }
+}
