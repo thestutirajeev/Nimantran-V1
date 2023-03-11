c7c2a7ebbbf6436218ef42e87c65b7a0fd67b375
Stuti Rajeev <stutirajeev5@gmail.com>
2023-03-11 16:47:59 +0530

    push

--- app/src/main/java/com/example/nimantran/ui/main/clientguests/MyGuestListFragment.kt
+++ app/src/main/java/com/example/nimantran/ui/main/clientguests/MyGuestListFragment.kt
@@ -5,18 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.example.nimantran.R
+import com.example.nimantran.adapters.MyGuestListAdapter
 import com.example.nimantran.databinding.FragmentMyGuestListBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MyGuestListFragment : Fragment() {
     private var _binding: FragmentMyGuestListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var db: FirebaseFirestore
+    private val myGuestListViewModel: MyGuestListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        db = FirebaseFirestore.getInstance()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMyGuestListBinding.inflate(inflater, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_guest_list, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        myGuestListViewModel.getGuests(db)// fetch data only
+        myGuestListViewModel.guests.observe(viewLifecycleOwner) { guests ->
+            if (guests.isNotEmpty()) {
+                binding.recyclerViewMyGuestList.adapter = MyGuestListAdapter(requireActivity(), {
+                    myGuestListViewModel.selectGuest(it)
+                }, {
+                    myGuestListViewModel.deleteGuest(db, it)
+                })
+
+                (binding.recyclerViewMyGuestList.adapter as MyGuestListAdapter).submitList(guests)
+            } else {
+                binding.recyclerViewMyGuestList.visibility = View.GONE
+            }
+            if (binding.swipeRefreshLayoutMyGuestList.isRefreshing) {
+                binding.swipeRefreshLayoutMyGuestList.isRefreshing = false
+            }
+        }
+        binding.swipeRefreshLayoutMyGuestList.setOnRefreshListener {
+            myGuestListViewModel.getGuests(db)
+        }
+        binding.floatingActionButtonAddGuest.setOnClickListener {
+            findNavController().navigate(R.id.action_myGuestListFragment_to_addGuestFragment)
+        }
     }
 
     companion object {
