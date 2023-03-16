c39e9fd12215141660a790a6dcf4a4d1fa00cd06
Stuti Rajeev <stutirajeev5@gmail.com>
2023-03-16 17:11:07 +0530

    push

--- app/src/main/java/com/example/nimantran/models/Template.kt
+++ app/src/main/java/com/example/nimantran/models/Template.kt
@@ -1,7 +1,6 @@
 package com.example.nimantran.models
 
 data class Template(
-    val id: Int = 0,
     val name: String = "",
-    val templateURL: String= "",
+    val url: String= "",
 )
--- app/src/main/java/com/example/nimantran/ui/main/addcard/CardEditorFragment.kt
+++ app/src/main/java/com/example/nimantran/ui/main/addcard/CardEditorFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.nimantran.R
+import androidx.fragment.app.activityViewModels
+import coil.load
 import com.example.nimantran.databinding.FragmentCardEditorBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import ja.burhanrashid52.photoeditor.PhotoEditor
+
 
 class CardEditorFragment : Fragment() {
     private var _binding: FragmentCardEditorBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: TemplateCardViewModel by activityViewModels()
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        db = Firebase.firestore
     }
 
     override fun onCreateView(
@@ -21,7 +30,21 @@ class CardEditorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCardEditorBinding.inflate(inflater, container, false)
+        binding.templateCardViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.selectedTemplate.value?.let {
+            val editor = PhotoEditor.Builder(requireContext(), binding.photoEditorView)
+                .setPinchTextScalable(true)
+                .build()
+            editor.setBrushDrawingMode(true)
+            binding.photoEditorView.source.load(it.url)
+        }
+
     }
 
 
