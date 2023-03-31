324a7f4d5e0bb27d26a68262aa09c5f1913aff0a
ishjaiswal7 <92149715+ishjaiswal7@users.noreply.github.com>
2023-03-31 11:44:14 +0530

    push 37

--- app/src/main/java/com/example/nimantran/ui/main/settings/MyProfileFragment.kt
+++ app/src/main/java/com/example/nimantran/ui/main/settings/MyProfileFragment.kt
@@ -1,26 +1,44 @@
 package com.example.nimantran.ui.main.settings
 
+import android.Manifest
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.example.nimantran.R
 import com.example.nimantran.databinding.FragmentMyProfileBinding
+import com.example.nimantran.ui.admin.gift.AddGiftFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
 
-class MyProfileFragment : Fragment() {
+class MyProfileFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private var _binding: FragmentMyProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+    private lateinit var userId: String
+    private lateinit var storage: FirebaseStorage
     private lateinit var db: FirebaseFirestore
-    //private val myProfileViewModel: MyProfileViewModel by viewModels()
+    private val myProfileViewModel: MyProfileViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         db = Firebase.firestore
     }
 
@@ -35,8 +53,128 @@ class MyProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //myProfileViewModel.getMyNotification(db)
+        disableEdit()
 
+//        myProfileViewModel.getClient(db)
+        val currUser = auth.currentUser
+        if (currUser != null) {
+            userId = currUser.uid
+            currUser.phoneNumber?.let { binding.editTextEditPhone.setText(it) }
+        }
+
+        binding.buttonEditUser.setOnClickListener {
+            enableEdit()
+        }
+
+        binding.buttonSaveEditUser.setOnClickListener {
+            disableEdit()
+            userId = currUser!!.uid
+            val name = binding.editTextEditName.text.toString().trim()
+            val phone = binding.editTextEditPhone.text.toString().trim()
+            val gender = when (binding.genderGroup.checkedRadioButtonId) {
+                R.id.radioFemale -> "Female"
+                R.id.radioMale -> "Male"
+                else -> "Unknown"
+            }
+
+            if (name.isEmpty()) {
+                binding.editTextEditName.error = "Please enter your name"
+                binding.editTextEditName.requestFocus()
+                return@setOnClickListener
+            }
+
+
+
+
+            myProfileViewModel.updateClient(db)
+        }
+
+        binding.imageViewEditUser.setOnClickListener {
+            selectImage()
+        }
+
+        binding.buttonCancelEditUser.setOnClickListener {
+            disableEdit()
+            myProfileViewModel.getClient(db)
+        }
+
+
+
+    }
+
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            binding.ImageViewUser.load(uri) {
+                crossfade(true)
+                transformations(CircleCropTransformation())
+                myProfileViewModel.uploadToFirebase(requireActivity(), storage, uri)
+            }
+        }
+    @AfterPermissionGranted(AddGiftFragment.REQUEST_IMAGE_GET)
+    private fun selectImage() {
+        if (EasyPermissions.hasPermissions(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            )
+        ) {
+            getContent.launch("image/*")
+        } else {
+            EasyPermissions.requestPermissions(
+                this, getString(R.string.permission_required),
+                122, Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        Toast.makeText(requireContext(), "Permission granted", Toast.LENGTH_SHORT).show()
+        selectImage()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun enableEdit(){
+        binding.apply {
+            buttonEditUser.isVisible = false
+            buttonSaveEditUser.isVisible = true
+            buttonCancelEditUser.isVisible = true
+            imageViewEditUser.isVisible = true
+            editTextEditName.isEnabled = true
+//            editTextEditPhone.isEnabled = true
+            genderGroup.isEnabled = true
+            radioFemale.isEnabled = true
+            radioMale.isEnabled = true
+        }
+    }
+
+    private fun disableEdit(){
+        binding.apply {
+            buttonEditUser.isVisible = true
+            buttonSaveEditUser.isVisible = false
+            buttonCancelEditUser.isVisible = false
+            imageViewEditUser.isVisible = false
+            editTextEditName.isEnabled = false
+            editTextEditPhone.isEnabled = false
+            genderGroup.isEnabled = false
+            radioFemale.isEnabled = false
+            radioMale.isEnabled = false
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/nimantran/ui/main/settings/MyProfileViewModel.kt
+++ app/src/main/java/com/example/nimantran/ui/main/settings/MyProfileViewModel.kt
@@ -1,4 +1,59 @@
 package com.example.nimantran.ui.main.settings
 
-class MyProfileViewModel {
+import android.net.Uri
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.nimantran.models.admin.Client
+import com.example.nimantran.ui.main.settings.MyProfileFragment.Companion.COLL_CLIENT
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MyProfileViewModel : ViewModel(){
+    private val _client = MutableLiveData<Client>()
+    val client: MutableLiveData<Client> = _client
+
+    private val _selectedClient = MutableLiveData<Client>()
+    val selectedClient: MutableLiveData<Client> = _selectedClient
+
+    private val _isLoading = MutableLiveData(false)
+    val isLoading: MutableLiveData<Boolean> = _isLoading
+
+    private val _isSaved = MutableLiveData(false)
+    val isSaved: MutableLiveData<Boolean> = _isSaved
+
+    private val _isImgUploaded = MutableLiveData(false)
+    val isImgUploaded: MutableLiveData<Boolean> = _isImgUploaded
+
+    private val _downloadUri = MutableLiveData<Uri>()
+    val downloadUri: MutableLiveData<Uri> = _downloadUri
+
+    fun getClient(db: FirebaseFirestore){
+        _isLoading.value = true
+        db.collection(COLL_CLIENT).document("client").get().addOnSuccessListener {
+            _isLoading.value = false
+            _client.value = it.toObject(Client::class.java)
+        }.addOnFailureListener {
+            _isLoading.value = false
+        }.addOnCanceledListener {
+            _isLoading.value = false
+        }
+    }
+
+    fun updateClient(db: FirebaseFirestore){
+        _isLoading.value = true
+        db.collection(COLL_CLIENT).document("client").set(_selectedClient.value!!).addOnSuccessListener {
+            _isLoading.value = false
+            _isSaved.value = true
+        }.addOnFailureListener {
+            _isLoading.value = false
+            _isSaved.value = false
+        }.addOnCanceledListener {
+            _isLoading.value = false
+            _isSaved.value = false
+        }
+    }
+
+    fun uploadToFirebase(requireActivity: FragmentActivity, storage: Any, uri: Uri?) {
+            TODO("Not yet implemented")
+    }
 }
