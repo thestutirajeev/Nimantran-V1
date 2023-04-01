951e9433c580be637f182077c9e9e0d1280c9e5e
Stuti Rajeev <stutirajeev5@gmail.com>
2023-04-02 00:10:35 +0530

    push

--- app/src/main/java/com/example/nimantran/models/user/MyOrder.kt
+++ app/src/main/java/com/example/nimantran/models/user/MyOrder.kt
@@ -1,5 +1,6 @@
 package com.example.nimantran.models.user
 
+import com.example.nimantran.models.admin.Gift
 import com.example.nimantran.models.user.Guest
 import com.example.nimantran.models.user.MyCards
 import java.util.Date
@@ -10,11 +11,10 @@ data class MyOrder(
     val orderDate: Date = Date(),
     //timestamp
     val orderStatus: String = "",
-    val orderTitle: String = "",
     //<gift,qty>
     //total amt
-    val orderImageSrc: String = "",
-    val card: MyCards = MyCards(),
+    //val card: MyCards = MyCards(),
+    val gift: Gift = Gift(),
     val guest: List<Guest> = emptyList(),//= listOf(Guest()),
     val paymentRefId: String = "",
 )
