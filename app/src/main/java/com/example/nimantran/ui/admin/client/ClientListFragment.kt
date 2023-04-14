bdbad78ab1da106691fde1e51b80c4c69b87f8d4
ishjaiswal7 <92149715+ishjaiswal7@users.noreply.github.com>
2023-04-14 10:59:03 +0530

    rolled back

--- app/src/main/java/com/example/nimantran/ui/admin/client/ClientListFragment.kt
+++ app/src/main/java/com/example/nimantran/ui/admin/client/ClientListFragment.kt
@@ -35,7 +35,6 @@ class ClientListFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
